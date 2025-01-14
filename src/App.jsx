117159e83ad359c95444ff139143780fddffa8f1
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-01-14 06:04:51 +0200

    Fix routing and setup for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,40 @@ import About from './components/About/About';
 import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
 import 'flowbite-react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
 
 
-let x = createBrowserRouter([
-  {path: "react-start-framework" , element: <Layout/>, children:[
-    {index: true , element: <Home/>},
-    {path: "about" , element: <About/>},
-    {path: "portfolio" , element: <Portfolio/>},
-    {path: "contact" , element: <Contact/>},
-    {path: "*" , element: <Notfound/>},
-  ]},
-])
+// let x = createBrowserRouter([
+//   {path: "react-start-framework" , element: <Layout/>, children:[
+//     {index: true , element: <Home/>},
+//     {path: "about" , element: <About/>},
+//     {path: "portfolio" , element: <Portfolio/>},
+//     {path: "contact" , element: <Contact/>},
+//     {path: "*" , element: <Notfound/>},
+//   ]},
+// ])
 
 function App() {
+  return (
+    <Router basename="/react-start-framework">
 
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<Notfound />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 
-  return <RouterProvider router={x}></RouterProvider>
+//--------------------------------anther answer--------------------------------
+  //return <RouterProvider router={x}></RouterProvider>
 }
 
 export default App
